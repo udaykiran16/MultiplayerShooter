1eb959700c38fdc8f984c2a8b7fdb7250d89626b
SaiPrabhu611 <41958569+SaiPrabhu611@users.noreply.github.com>
2019-03-18 21:31:34 +0530

    menu

--- MultiplayerShooter/Assets/Invector-3rdPersonController/Shooter/Scripts/network.cs
+++ MultiplayerShooter/Assets/Invector-3rdPersonController/Shooter/Scripts/network.cs
@@ -101,8 +101,8 @@ namespace UnderdogCity
             Debug.Log("Master: " + PhotonNetwork.IsMasterClient + " | Players In Room: " + PhotonNetwork.CurrentRoom.PlayerCount + " | RoomName: " + PhotonNetwork.CurrentRoom.Name + " Region: " + PhotonNetwork.CloudRegion);
             //if(PhotonNetwork.IsMasterClient && SceneManager.GetActiveScene().name != "Network")
             //    PhotonNetwork.LoadLevel("Network");
-            if (PhotonNetwork.IsMasterClient && SceneManager.GetActiveScene().name != "Main")
-                PhotonNetwork.LoadLevel("Main");
+            if (PhotonNetwork.IsMasterClient && SceneManager.GetActiveScene().name != "TPScene")
+                PhotonNetwork.LoadLevel("TPScene");
         }
     }
 }
